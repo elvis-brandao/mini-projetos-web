5b8acfb2d2703289ce39b2560060264834ae48f8
elvis-brandao <elvis.brandao@outlook.com>
2021-12-28 17:49:44 -0300

    Projeto Random Choice OK

---
+++ 13_random_choice/script.js
@@ -0,0 +1,61 @@
+const tagsEl = document.getElementById('tags'); //Note the plural
+const textarea = document.getElementById('textarea');
+
+textarea.focus();
+textarea.addEventListener('keyup', e =>{
+    createTags(e.target.value);
+
+    if(e.key === 'Enter'){
+        setTimeout(() => e.target.value = '', 10);
+        
+        randomSelect();
+    }
+});
+
+function createTags(input){
+    const tags = input.split(',')
+    .filter(tag => tag.trim() !== '')
+    .map(tag => tag.trim());
+
+    tagsEl.innerHTML = '';
+
+    tags.forEach( tag => {
+        const tagEl = document.createElement('span'); //Note the singular
+        tagEl.classList.add('tag');
+        tagEl.innerText = tag;
+        tagsEl.appendChild(tagEl); //Note the plural/singular
+    })
+};
+
+function randomSelect(){
+    const times = 30;
+    
+    const interval = setInterval(() => {
+        const randomTag = pickRandomTag();
+        
+        highlightTag(randomTag);
+        setTimeout(() => unHighlightTag(randomTag), 100);
+    }, 100);
+
+    setTimeout(() => {
+        clearInterval(interval);
+
+        setTimeout(() => {
+            const randomTag = pickRandomTag();
+            highlightTag(randomTag);
+        }, 100);
+    }, times * 30);
+};
+
+function pickRandomTag(){
+    const tags = document.querySelectorAll('.tag');
+    return tags[Math.floor(Math.random() * tags.length)]
+};
+
+function highlightTag(tag){
+    tag.classList.add('highlight');
+};
+
+function unHighlightTag(tag){
+    tag.classList.remove('highlight');
+};
