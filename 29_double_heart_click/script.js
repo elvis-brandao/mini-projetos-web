1a621157e6f833267c833e7b26a84f4ce65e3e2a
elvis-brandao <elvis.brandao@outlook.com>
2022-01-06 14:08:21 -0300

    Projeto Double Click to Heart OK

---
+++ 29_double_heart_click/script.js
@@ -0,0 +1,31 @@
+const loveMe = document.querySelector('.loveMe');
+const times = document.querySelector('#times');
+
+let timesClicked = 0;
+
+loveMe.addEventListener('dblclick', e => createHeart(e));
+
+function createHeart(event){
+    const heart = document.createElement('i');
+    heart.classList.add('fas');
+    heart.classList.add('fa-heart');
+
+    const x = event.clientX;
+    const y = event.clientY;
+
+    const leftOffset = event.target.offsetLeft;
+    const topOffset = event.target.offsetTop;
+
+    const xInside = x - leftOffset;
+    const yInside = y - topOffset;
+    
+
+    heart.style.top = `${yInside}px`;
+    heart.style.left = `${xInside}px`;
+
+    loveMe.appendChild(heart);
+
+    times.innerHTML = ++timesClicked;
+
+    setTimeout(() => heart.remove(), 1000);
+}
