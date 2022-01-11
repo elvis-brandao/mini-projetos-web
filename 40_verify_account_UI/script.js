2fbb19e4dc0f0709fa42b402e83305205cc9005d
elvis-brandao <elvis.brandao@outlook.com>
2022-01-11 18:04:00 -0300

    Projeto Verify Account UI OK

---
+++ 40_verify_account_UI/script.js
@@ -0,0 +1,14 @@
+const codes = document.querySelectorAll('.code');
+
+codes[0].focus();
+
+codes.forEach((code, idx) => {
+    code.addEventListener('keydown', e => {
+        if(e.key >= 0 && e.key <= 9){
+            codes[idx].value = '';
+            setTimeout(() => codes[idx + 1].focus(), 10);
+        }else if(e.key === 'Backspace' || e.key === 'Delete'){
+            setTimeout(() => codes[idx - 1].focus(), 10);
+        };
+    });
+});
